debc780dd7a439213e262dae1f9ff41e0b7cd334
Saint. Clever <parchment@gmx.com>
2024-03-20 10:28:20 -0400

    Add Query and Path Parameters

--- projects/books/books.py
+++ projects/books/books.py
@@ -24,6 +24,7 @@ async def read_all_books():
 
 
 # Path Parameter
+# http://127.0.0.1:8000/books/title%20one
 @app.get("/books/{book_title}")
 async def read_all_books(book_title: str):
     for book in BOOKS:
@@ -32,3 +33,28 @@ async def read_all_books(book_title: str):
 
 
 # Query Parameter
+# http://127.0.0.1:8000/books/?category=science
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = []
+
+    for book in BOOKS:
+        if book.get("category").casefold() == category.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
+
+
+# Path and Query Parameter
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+
+    for book in BOOKS:
+        if (
+            book.get("author").casefold() == book_author.casefold()
+            and book.get("category").casefold() == category.casefold()
+        ):
+            books_to_return.append(book)
+
+    return books_to_return
